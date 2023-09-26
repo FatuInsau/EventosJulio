1559820c4c2a3bbddce792d1b33f92bd1510cda8
FatuInsau <insaufaty@gmail.com>
2023-09-26 15:03:21 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,28 @@
+let $divCaja = document.getElementById('caja');
+
+fetch('https://rickandmortyapi.com/api/location')
+.then( (data) => {
+    return data.json();
+})
+.then( (data)=> {
+    let lugares = data.results;
+    for (let i=0; i<lugares.length; i++){
+        $divCaja.innerHTML += `<div class="lugar">
+                                    <h2>Nombre: ${lugares[i].name}</h2>
+                                    <p>Tipo: ${lugares[i].type}</p>
+                                    <p>Dimensión: ${lugares[i].dimension}</p>
+                                </div>`
+    };
+})
+.catch((error)=>{
+    console.log(error)
+});
+
+let $h1 = document.querySelector('h1');
+let $boton = document.getElementById('boton');
+
+function cambiarColor () {
+    $h1.classList.toggle('rojo');
+};
+
+$boton.addEventListener('click', cambiarColor);
